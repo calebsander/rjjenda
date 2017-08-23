4378b16ed658de12aa54088316080f4d2ee08213
Alex Lew <alexl.mail@gmail.com>
2017-08-23 11:38:35 -0400

    First pass at prettier Login page (fixes #15)
    (Willing to change the photo :-)

--- frontend/components/Login.vue
+++ frontend/components/Login.vue
@@ -1,11 +1,24 @@
 <template>
-	<md-layout md-align='center'>
-		<md-layout md-flex md-column md-vertical-align='center'>
-			<md-layout md-flex>
-				<md-button href='/auth' class='md-raised md-primary'>Sign in with Google</md-button>
-			</md-layout>
-		</md-layout>
-	</md-layout>
+	<div id="sign-in">
+		<md-card md-with-hover>
+			<md-card-header>
+				<div class="md-title">RJJenda</div>
+				<div class="md-subhead">Commonwealth Workload Manager</div>
+			</md-card-header>
+
+			<md-card-media>
+				<img src="/assets/login-photo.jpg" alt="People">
+			</md-card-media>
+
+			<md-card-content>
+				Please use your Commonwealth Google account to sign into the workload manager.
+			</md-card-content>
+
+			<md-card-actions>
+				<md-button href='/auth'>Sign in with Google</md-button>
+			</md-card-actions>
+		</md-card>
+	</div>
 </template>
 
 <script lang='ts'>
@@ -15,3 +28,14 @@
 	@Component
 	export default class LoginComponent extends Vue {}
 </script>
+
+<style lang="sass">
+	body
+		background: darkred
+	#sign-in
+		display: flex
+	#sign-in > div
+		margin: auto
+		margin-top: 100px
+		width: 450px
+</style>
