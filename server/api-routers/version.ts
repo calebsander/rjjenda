0d979d469294f29f804e3743a30c428c5fcc6cea
Caleb Sander <caleb.sander@gmail.com>
2017-08-24 17:04:04 -0400

    Fixed version info not working on Windows

--- server/api-routers/version.ts
+++ server/api-routers/version.ts
@@ -5,7 +5,7 @@ import {success} from '../api-respond'
 const router = express.Router()
 router.get('/', (_, res) => {
 	const response: VersionInfo = {
-		app: process.env.npm_package_version!,
+		app: require('../../package.json').version,
 		node: process.versions.node,
 		sequelize: require('sequelize').version,
 		typescript: require('typescript').version
