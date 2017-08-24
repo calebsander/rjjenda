47e00699ae4e9758f1a338778fb881019f2f4c04
Caleb Sander <caleb.sander@gmail.com>
2017-08-24 17:01:16 -0400

    Fixed #2
    Only importing specific JS modules did not decrease the bundle size

--- frontend/index.ts
+++ frontend/index.ts
@@ -1,6 +1,25 @@
 import Vue from 'vue'
 import VueMaterial from 'vue-material'
-import 'vue-material/dist/vue-material.css'
+import 'vue-material/dist/components/mdCore/index.css'
+import 'vue-material/dist/components/mdBackdrop/index.css'
+import 'vue-material/dist/components/mdButton/index.css'
+import 'vue-material/dist/components/mdCard/index.css'
+import 'vue-material/dist/components/mdCheckbox/index.css'
+import 'vue-material/dist/components/mdChips/index.css'
+import 'vue-material/dist/components/mdDialog/index.css'
+import 'vue-material/dist/components/mdFile/index.css'
+import 'vue-material/dist/components/mdIcon/index.css'
+import 'vue-material/dist/components/mdInputContainer/index.css'
+import 'vue-material/dist/components/mdLayout/index.css'
+import 'vue-material/dist/components/mdList/index.css'
+import 'vue-material/dist/components/mdMenu/index.css'
+import 'vue-material/dist/components/mdSelect/index.css'
+import 'vue-material/dist/components/mdSidenav/index.css'
+import 'vue-material/dist/components/mdSpinner/index.css'
+import 'vue-material/dist/components/mdSwitch/index.css'
+import 'vue-material/dist/components/mdTable/index.css'
+import 'vue-material/dist/components/mdToolbar/index.css'
+import 'vue-material/dist/components/mdTooltip/index.css'
 import VueRouter from 'vue-router'
 import AdminView from './components/AdminView.vue'
 import AdvisorView from './components/AdvisorView.vue'
@@ -12,7 +31,27 @@ import StudentView from './components/StudentView.vue'
 import TeacherHome from './components/TeacherHome.vue'
 import TeacherView from './components/TeacherView.vue'
 
-Vue.use(VueMaterial)
+Vue.use(VueMaterial.MdCore)
+Vue.use(VueMaterial.MdBackdrop)
+Vue.use(VueMaterial.MdButton)
+Vue.use(VueMaterial.MdCard)
+Vue.use(VueMaterial.MdCheckbox)
+Vue.use(VueMaterial.MdChips)
+Vue.use(VueMaterial.MdDialog)
+Vue.use(VueMaterial.MdFile)
+Vue.use(VueMaterial.MdIcon)
+Vue.use(VueMaterial.MdInputContainer)
+Vue.use(VueMaterial.MdLayout)
+Vue.use(VueMaterial.MdList)
+Vue.use(VueMaterial.MdMenu)
+Vue.use(VueMaterial.MdSelect)
+Vue.use(VueMaterial.MdSidenav)
+Vue.use(VueMaterial.MdSpinner)
+Vue.use(VueMaterial.MdSwitch)
+Vue.use(VueMaterial.MdTable)
+Vue.use(VueMaterial.MdToolbar)
+Vue.use(VueMaterial.MdTooltip)
+
 Vue.use(VueRouter)
 
 Vue.material.registerTheme('default', {
